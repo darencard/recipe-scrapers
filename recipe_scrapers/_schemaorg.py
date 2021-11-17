0bc5d894752d3867c6345c6c82eff5160846158c
Daren Card <daren.card@gmail.com>
2021-11-17 14:36:05 -0500

    changed new line to && in instructions

--- recipe_scrapers/_schemaorg.py
+++ recipe_scrapers/_schemaorg.py
@@ -180,7 +180,7 @@ class SchemaOrg:
                     schema_instruction_item
                 )
 
-            return "\n".join(
+            return "&&".join(
                 normalize_string(instruction) for instruction in instructions_gist
             )
 
